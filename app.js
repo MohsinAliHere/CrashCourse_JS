7bd11f98c2d1ed0c3e438523e876cce469f50fa6
“mohsinalihere” <mohsinalihere@gmail.com>
2023-04-07 05:29:45 -0700

    first assigment done

---
+++ app.js
@@ -0,0 +1,96 @@
+// 1.
+// =================================================
+// let num1 = parseInt(prompt("Enter number 1"))
+// let num2 = parseInt(prompt("Enter number 2"))
+// console.log("Number 1 :- ", num1)
+// console.log("Number 2 :- ", num2)
+// if (num1 > num2) {
+//     console.log("Answer Number 1 is larger than Number 2 :- ", num1)
+// } else if (num1 == num2) {
+//     console.log("Answer Number 1 and  Number 2 are equal :- ", num1 , num2)
+// }
+// else {
+//     console.log("Answer Number 2 is larger than Number 1 :- ", num2)
+// }
+
+// 2.
+// =================================================
+// let num1 = parseInt(prompt("Enter Number "))
+// if (num1>=0) {
+//     alert("the sign is + ")
+// }else{
+//     alert("the sign is - ")
+
+// }
+
+// 3.
+// =================================================
+// const num1 = parseInt(prompt("Enter number 1"));
+// const num2 = parseInt(prompt("Enter number 2"));
+// const num3 = parseInt(prompt("Enter number 3"));
+// const num4 = parseInt(prompt("Enter number 4"));
+// const num5 = parseInt(prompt("Enter number 5"));
+// let temp = num1;
+// if (num2 > temp) {
+//   temp = num2;
+// }
+// if (num3 > temp) {
+//   temp = num3;
+// }
+// if (num4 > temp) {
+//   temp = num4;
+// }
+// if (num5 > temp) {
+//   temp = num5;
+// }
+// console.log(temp);
+
+// 4.
+// =================================================
+// for (let index = 0; index < 16; index++) {
+//     if (index % 2 == 0) {
+//         console.log(index + " is Even")
+//     } else if (index % 2 !== 0) {
+//         console.log(index + " is Odd")
+//     }
+// }
+
+// 5
+// =================================================
+// =================================================
+// const UserMask = +prompt("Enter the user mask to generate grade");
+// if (UserMask < 60) {
+//   console.log("F");
+// } else if (UserMask < 70) {
+//   console.log("D");
+// } else if (UserMask < 80) {
+//   console.log("C");
+// } else if (UserMask < 90) {
+//   console.log("B");
+// } else if (UserMask <= 100) {
+//   console.log("A");
+// } else {
+//   console.log("Invalid User Mask");
+// }
+
+// 6;
+// =================================================
+// for (let i = 1; i <= 100; i++) {
+//     if (i % 3 === 0 && i % 5 === 0) {
+//       console.log("FizzBuzz" , i);
+//     } else if (i % 3 === 0) {
+//       console.log("Fizz" , i);
+//     } else if (i % 5 === 0) {
+//       console.log("Buzz" , i);
+//     }
+//   }
+
+// 7.
+// =================================================
+// for (let i = 1; i <= 5; i++) {
+//   let row = "";
+//   for (let j = 1; j <= i; j++) {
+//     row += "* ";
+//   }
+//   console.log(row);
+// }
